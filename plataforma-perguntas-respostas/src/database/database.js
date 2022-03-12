4e5be5fe2b52547ea669e86ad9229e97b5a5c737
franSborges <borgesfran988@gmail.com>
2022-03-12 15:41:56 -0300

    third commit

--- plataforma-perguntas-respostas/src/database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('questionsAndanswers', 'root', '306433', {
-  host: 'localhost',
-  dialect: 'mysql'
-})
-
-module.exports = connection;
